e17118bb4f1e8068823395290d6755714ffdb5a4
julis <617669559@qq.com>
2018-02-10 20:35:54 +0800

    julis
    jls

--- application/mobile/controller/AgentBackend.php
+++ application/mobile/controller/AgentBackend.php
@@ -1,13 +1,6 @@
 <?php
 /**
- * oscshop2 B2C电子商务系统
- *
- * ==========================================================================
- * @link      http://www.oscshop.cn/
- * @copyright Copyright (c) 2015-2016 oscshop.cn. 
- * @license   http://www.oscshop.cn/license.html License
- * ==========================================================================
- *
+
  * @author    李梓钿
  *
  */
