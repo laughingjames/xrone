ccbfbf7479942c67fbf0d71f7986c759c3030f0d
julis <617669559@qq.com>
2018-03-21 15:43:05 +0800

    hi

--- application/mobile/controller/App.php
+++ application/mobile/controller/App.php
@@ -115,5 +115,57 @@ class App extends MobileBase
         return $this->fetch();
     }
 
+    /**
+     * 展示房源信息
+     * @return mixed
+     */
+    public function take_out(){
+        $list=[];
+        $list= Db::name('house_renting')
+            ->paginate(config('page_num'));
+
+        $this->assign('top_title','租房/HouseRenting');
+        $this->assign('SEO',['title'=>'租房/HouseRenting-'.config('SITE_TITLE')]);
+        $this->assign('flag','search');
+        $this->assign('houselist',$list);
+
+        return $this->fetch();
+    }
+    //兼职详情
+    function take_out_detail(){
+
+        if(request()->isPost()){
+            $house_id=(int)input('post.id');
+            cookie('jump_url',request()->url(true).'/id/'.$house_id);
+            $house_title=Db::name('house_renting')->field('title')->where('id',$house_id)->find()['title'];
+            if(empty($house_title)){
+                return ['error'=>'房屋信息不存在！！/House info does not exist!!'];
+            }
+            $uid=user('uid');
+            if(!$uid){
+                return ['error'=>'请先登录/Please Log on First! ','url'=>"/mobile/login/login"];
+            }
+            if(Db::name('apply_house')->where(array('hid'=>$house_id,'uid'=>$uid))->find()){
+                return ['error'=>'你已经申请/You already applied.'];
+            }
+            Db::name('house_renting')->where('id',$house_id)->setInc('apply_counts');
+            Db::name('apply_house')->insert(array('uid'=>$uid, 'hid'=>$house_id));
+            storage_user_action($uid,user('username'),config('FRONTEND_USER'),'申请了：'.$house_title.'房屋');
+            return ['success'=>'申请成功/Apply Success', 'url'=>"/mobile/app/house_renting"];
+        }
+
+
+        $house_id=input('param.id');
+        $house_detail=Db::name('house_renting')->where('id',$house_id)->find();
+        if(empty($house_detail)){
+            $this->error('房屋信息不存在！！/House renting info do not exist!!');
+        }
+
+        $images=Db::name('house_renting_description_image')->where('house_id',$house_id)->select();
+        $this->assign('image',$images);
+        $this->assign('top_title',$house_detail['title']);
+        $this->assign('house',$house_detail);
+        return $this->fetch();
+    }
 
 }
